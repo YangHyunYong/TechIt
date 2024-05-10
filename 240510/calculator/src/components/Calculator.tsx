8f2d652097a2064fea7323eace2759f5fae70869
YangHyunYong <43340172+YangHyunYong@users.noreply.github.com>
2024-05-10 17:23:03 +0900

    css 수정

--- 240510/calculator/src/components/Calculator.tsx
+++ 240510/calculator/src/components/Calculator.tsx
@@ -11,56 +11,59 @@ const Calculator: FC = () => {
   const [operator, setOperator] = useState("");
 
   return (
-    <div className="bg-gray-300 p-4 shadow-md">
-      <Result result={result}></Result>
-      <div className=" grid grid-cols-4 gap-2">
-        <ButtonNumber name="1" result={result} setResult={setResult} />
-        <ButtonNumber name="2" result={result} setResult={setResult} />
-        <ButtonNumber name="3" result={result} setResult={setResult} />
-        <ButtonOperator
-          name="+"
-          result={result}
-          setTemp={setTemp}
-          setResult={setResult}
-          setOperator={setOperator}
-        />
-        <ButtonNumber name="4" result={result} setResult={setResult} />
-        <ButtonNumber name="5" result={result} setResult={setResult} />
-        <ButtonNumber name="6" result={result} setResult={setResult} />
-        <ButtonOperator
-          name="-"
-          result={result}
-          setTemp={setTemp}
-          setResult={setResult}
-          setOperator={setOperator}
-        />
-        <ButtonNumber name="7" result={result} setResult={setResult} />
-        <ButtonNumber name="8" result={result} setResult={setResult} />
-        <ButtonNumber name="9" result={result} setResult={setResult} />
-        <ButtonOperator
-          name="*"
-          result={result}
-          setTemp={setTemp}
-          setResult={setResult}
-          setOperator={setOperator}
-        />
-        <ButtonEnter
-          name="Enter"
-          operator={operator}
-          result={result}
-          temp={temp}
-          setResult={setResult}
-          setTemp={setTemp}
-        />
-        <ButtonNumber name="0" result={result} setResult={setResult} />
-        <ButtonDelete name="Del" result={result} setResult={setResult} />
-        <ButtonOperator
-          name="/"
-          result={result}
-          setTemp={setTemp}
-          setResult={setResult}
-          setOperator={setOperator}
-        />
+    <div className="flex flex-col">
+      <div className="bg-gray-950 h-[45px] rounded-md"></div>
+      <div className="bg-gray-300 p-4 shadow-md">
+        <Result result={result}></Result>
+        <div className=" grid grid-cols-4 gap-2">
+          <ButtonNumber name="1" result={result} setResult={setResult} />
+          <ButtonNumber name="2" result={result} setResult={setResult} />
+          <ButtonNumber name="3" result={result} setResult={setResult} />
+          <ButtonOperator
+            name="+"
+            result={result}
+            setTemp={setTemp}
+            setResult={setResult}
+            setOperator={setOperator}
+          />
+          <ButtonNumber name="4" result={result} setResult={setResult} />
+          <ButtonNumber name="5" result={result} setResult={setResult} />
+          <ButtonNumber name="6" result={result} setResult={setResult} />
+          <ButtonOperator
+            name="-"
+            result={result}
+            setTemp={setTemp}
+            setResult={setResult}
+            setOperator={setOperator}
+          />
+          <ButtonNumber name="7" result={result} setResult={setResult} />
+          <ButtonNumber name="8" result={result} setResult={setResult} />
+          <ButtonNumber name="9" result={result} setResult={setResult} />
+          <ButtonOperator
+            name="*"
+            result={result}
+            setTemp={setTemp}
+            setResult={setResult}
+            setOperator={setOperator}
+          />
+          <ButtonEnter
+            name="Enter"
+            operator={operator}
+            result={result}
+            temp={temp}
+            setResult={setResult}
+            setTemp={setTemp}
+          />
+          <ButtonNumber name="0" result={result} setResult={setResult} />
+          <ButtonDelete name="Del" result={result} setResult={setResult} />
+          <ButtonOperator
+            name="/"
+            result={result}
+            setTemp={setTemp}
+            setResult={setResult}
+            setOperator={setOperator}
+          />
+        </div>
       </div>
     </div>
   );
